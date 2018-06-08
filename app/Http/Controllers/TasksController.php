bd3518922e671a9b90b7bdbd898ab3cca1f8e775
nakkygyo <natsukigyotoku.yahoo.co.jp>
2018-06-08 06:08:56 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -47,11 +47,12 @@ class TasksController extends Controller
     {
         $this->validate($request, [
             'content'=> 'required|max:255',
-            // "staus"=>"required|max:10",
+            "status"=>"required|max:10",
             ]);
         
         $task = new Task;
         $task->content = $request->content;
+        $task->status = $request->status;
         $task->save();
         
         return redirect("/");
@@ -99,11 +100,12 @@ class TasksController extends Controller
     {
         $this->validate($request, [
             'content'=> 'required|max:255',
-            // "status"=> "required|max:10",
+            "status"=> "required|max:10",
             ]);
         
         $task = Task::find($id);
         $task->content = $request->content;
+        $task->status = $request->status;
         $task->save();
         
         return redirect("/"); 
