5c05f616f899635ab6161873bb2b79342829c65c
nakkygyo <natsukigyotoku.yahoo.co.jp>
2018-06-22 02:49:19 +0000

    change login again

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
 
 @section("content")
     <div class="text-center">
-        <h1>Sign up</h1>
+        <h1>Log in</h1>
     </div>
     <div class="row">
         <div class="col-md-6 col-md-offset-3">
